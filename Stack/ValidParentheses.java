3366549698d6a2046cf0bc0d4921bbfdbbe99cd7
shaikhahmad0968 <ahmadshaikh0968@gmail.com>
2026-05-04 02:13:01 +0530

    8 and 9

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char ch : s.toCharArray()) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+
+                if((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')){
+                        return false;
+                    }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+public class ValidParentheses{
+    public static void main(String[] args) {
+        String s = "([{}])";
+        Solution obj = new Solution();
+        System.out.println(obj.isValid(s));
+    }
+}
